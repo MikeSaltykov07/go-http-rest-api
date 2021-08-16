6e15335f674e490b470ce0ab13a57df97c5ae1b0
Mike <mouse-saltykov@mail.ru>
2021-08-16 03:19:54 +0300

    add validation users, not work: make test

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.4.1
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
 	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -12,15 +12,13 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
 
-func TestUserRepository_FinByEmail(t *testing.T) {
+func TestUserRepository_FindByEmail(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
@@ -28,11 +26,10 @@ func TestUserRepository_FinByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
-
-	u, err := s.User().FindByEmail(email)
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 
